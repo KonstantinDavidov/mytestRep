acf472f1bb428d72c4f434ab5705c8d949a731b4
Vyacheslav Akeyev <Vyacheslav.Akeyev@opensoftdev.ru>
2013-05-06 19:27:46 +0400

    Reissuance dialog modified.

---
+++ Sources/FACCTS.Controls/Views/ReissuanceDialogView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FACCTS.Controls
+{
+	/// <summary>
+	/// Interaction logic for ReissuanceDialogView.xaml
+	/// </summary>
+	public partial class ReissuanceDialogView : Window
+	{
+		public ReissuanceDialogView()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+	}
+}
