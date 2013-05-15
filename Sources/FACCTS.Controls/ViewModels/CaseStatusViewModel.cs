66aa9570f25f82e57a65e5bca1890ab66b675f40
Vyacheslav Akeyev <Vyacheslav.Akeyev@opensoftdev.ru>
2013-05-15 17:07:17 +0400

    Infrastructure for searching the Cases/Hearings partially done.

--- Sources/FACCTS.Controls/ViewModels/CaseStatusViewModel.cs
+++ Sources/FACCTS.Controls/ViewModels/CaseStatusViewModel.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ReactiveUI;
 
 namespace FACCTS.Controls.ViewModels
 {
@@ -16,6 +17,170 @@ namespace FACCTS.Controls.ViewModels
             this.DisplayName = "Case Status";
         }
 
+        private string _caseNumber;
+        public string CaseNumber
+        {
+            get
+            {
+                return _caseNumber;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _caseNumber, value);
+            }
+        }
 
+        private DateTime? _firstActivityStartDate;
+        public DateTime? FirstActivityStartDate
+        {
+            get
+            {
+                return _firstActivityStartDate;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _firstActivityStartDate, value);
+            }
+        }
+
+        private DateTime? _lastActivityStartDate;
+        public DateTime? LastActivityStartDate
+        {
+            get
+            {
+                return _lastActivityStartDate;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _lastActivityStartDate, value);
+            }
+        }
+
+        private DateTime? _firstActivityEndDate;
+        public DateTime? FirstActivityEndDate
+        {
+            get
+            {
+                return _firstActivityEndDate;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _firstActivityEndDate, value);
+            }
+        }
+
+        private DateTime? _lastActivityEndDate;
+        public DateTime? LastActivityEndDate
+        {
+            get
+            {
+                return _lastActivityEndDate;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _lastActivityEndDate, value);
+            }
+        }
+
+        private string _ccpor_Id;
+        public string CCPOR_ID
+        {
+            get
+            {
+                return _ccpor_Id;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _ccpor_Id, value);
+            }
+        }
+
+        private string _party1FirstName;
+        public string Party1FirstName
+        {
+            get
+            {
+                return _party1FirstName;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _party1FirstName, value);
+            }
+        }
+
+        private string _party1MiddleName;
+        public string Party1MiddleName
+        {
+            get
+            {
+                return _party1MiddleName;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _party1MiddleName, value);
+            }
+        }
+
+        private string _party1LastName;
+        public string Party1LastName
+        {
+            get
+            {
+                return _party1LastName;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _party1LastName, value);
+            }
+        }
+
+        private string _party2FirstName;
+        public string Party2FirstName
+        {
+            get
+            {
+                return _party2FirstName;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _party2FirstName, value);
+            }
+        }
+
+        private string _party2MiddleName;
+        public string Party2MiddleName
+        {
+            get
+            {
+                return _party2MiddleName;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _party2MiddleName, value);
+            }
+        }
+
+        private string _party2LastName;
+        public string Party2LastName
+        {
+            get
+            {
+                return _party2LastName;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _party2LastName, value);
+            }
+        }
+
+        public void Clear()
+        {
+            //TODO implement Clear()
+        }
+
+        public void Find()
+        {
+            //TODO implement Find()
+        }
     }
 }
