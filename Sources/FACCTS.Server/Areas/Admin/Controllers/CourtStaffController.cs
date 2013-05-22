86ed10087aede111f397d4f96eb4c2252f05c5bd
Mikhail Petrov <mikhail.petrov@opensoftdev.ru>
2013-05-22 18:37:30 +0400

    CourtStaff - show llist users, editting user

--- Sources/FACCTS.Server/Areas/Admin/Controllers/CourtStaffController.cs
+++ Sources/FACCTS.Server/Areas/Admin/Controllers/CourtStaffController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using FACCTS.Server.Areas.Admin.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace FACCTS.Server.Areas.Admin
 {
@@ -16,7 +18,27 @@ namespace FACCTS.Server.Areas.Admin
             return RedirectToAction("Profile");
         }
 
-        public ActionResult Profile()
+        public ActionResult Profile(string userName)
+        {
+            ViewBag.userName = userName;
+            EditUser editUser = new EditUser();
+
+            if (!string.IsNullOrEmpty(userName))
+            {
+                MembershipUser user = Membership.GetUser(userName);
+
+                if (user != null)
+                {
+                    editUser.UserName = user.UserName;
+                    editUser.Email = user.Email;
+                }
+            }
+
+            return View(editUser);
+        }
+
+        [HttpPost]
+        public ActionResult Profile(EditUser user)
         {
             return View();
         }
@@ -29,6 +51,30 @@ namespace FACCTS.Server.Areas.Admin
         public ActionResult UserRole()
         {
             return View();
+        }
+
+        public PartialViewResult UsersList(string userName)
+        {
+            MembershipUserCollection users = Membership.GetAllUsers();
+
+            ViewBag.userName = userName;
+
+            return PartialView(users);
+        }
+
+        public ActionResult CreateUser(string userName)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RemoveUser(string userName)
+        {
+            /*
+             * remove user
+             */
+
+            return RedirectToAction("Index");
         }
     }
 }
