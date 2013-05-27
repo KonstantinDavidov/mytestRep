c77a2ed913098c085a1ee6df4f548951ab108a13
tatyana.fedorova <tatyana.fedorova@opensoftdev.ru>
2013-05-27 18:14:49 +0400

    test

--- Sources/FACCTS.Server.Database/dbo/Tables/ClientCertificates.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[ClientCertificates] (
-    [Id] [int] NOT NULL IDENTITY,
-    [UserName] [nvarchar](max) NOT NULL,
-    [Thumbprint] [nvarchar](max) NOT NULL,
-    [Description] [nvarchar](max) NOT NULL,
-    CONSTRAINT [PK_dbo.ClientCertificates] PRIMARY KEY ([Id])
-)
--- Sources/FACCTS.Server.Database/dbo/Tables/KeyMaterialConfiguration.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[KeyMaterialConfiguration] (
-    [Id] [int] NOT NULL IDENTITY,
-    [SigningCertificateName] [nvarchar](max),
-    [DecryptionCertificateName] [nvarchar](max),
-    [RSASigningKey] [nvarchar](max),
-    [SymmetricSigningKey] [nvarchar](max),
-    CONSTRAINT [PK_dbo.KeyMaterialConfiguration] PRIMARY KEY ([Id])
-)
