65ffb49b5c7a6b72b62f29770972f5dc0acb8cb4
TatyanaFedorova <tatyana.fedorova@opensoftdev.ru>
2013-05-29 19:27:37 +0400

    Some attempts with using custom membership provider

---
+++ Sources/FACCTS.Server/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using FACCTS.Server.Model.Membership;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Transactions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using FACCTS.Server.Models;
+//using DotNetOpenAuth.AspNet;
+//using Microsoft.Web.WebPages.OAuth;
+//using WebMatrix.WebData;
+
+
+namespace FACCTS.Server.Areas.Admin.Controllers
+{
+    //[Authorize]
+    //[InitializeSimpleMembership]
+    public class AccountController : Controller
+    {
+        public string Index()
+        {
+            return "testc";
+        }
+        //
+        // GET: /Account/Login
+        
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+    }
+}
+
