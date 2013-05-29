9b00eac5424d808246e4acb9d5bda38577fde945
VyacheslavAkeyev <Vyacheslav.Akeyev@opensoftdev.ru>
2013-05-29 20:23:39 +0400

    Model for the WPF client added.

---
+++ Sources/FACCTS.Services/DataRepository/Core/EFUnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using FACCTS.Services.DataRepository.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Objects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FACCTS.Services.DataRepository.Core
+{
+    public class EFUnitOfWork : IUnitOfWork, IDisposable
+    {
+        public ObjectContext Context { get; private set; }
+
+        public EFUnitOfWork(ObjectContext context)
+        {
+            Context = context;
+            context.ContextOptions.LazyLoadingEnabled = true;
+        }
+
+        public void Commit()
+        {
+            Context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            if (Context != null)
+            {
+                Context.Dispose();
+                Context = null;
+            }
+
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Sources/FACCTS.Services/DataRepository/Core/Interfaces/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FACCTS.Services.DataRepository.Core.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> GetQuery();
+
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> where);
+        T Single(Expression<Func<T, bool>> where);
+        T First(Expression<Func<T, bool>> where);
+
+        void Delete(T entity);
+        void Add(T entity);
+        void Attach(T entity);
+        void SaveChanges();
+    }
+}
