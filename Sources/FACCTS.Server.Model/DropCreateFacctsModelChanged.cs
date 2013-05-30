4afd6d51fb690dfd3b800af2b9a8190c1509ec94
VyacheslavAkeyev <Vyacheslav.Akeyev@opensoftdev.ru>
2013-05-30 16:47:48 +0400

    Additional db initializer added.

---
+++ Sources/FACCTS.Server.Model/DropCreateFacctsModelChanged.cs
@@ -0,0 +1,19 @@
+﻿using FACCTS.Server.Model.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FACCTS.Server.Model
+{
+    public class DropCreateFacctsModelChanged : DropCreateDatabaseIfModelChanges<DatabaseContext>
+    {
+        protected override void Seed(DatabaseContext context)
+        {
+            DatabaseHelper.SeedDatabase(context);
+            base.Seed(context);
+        }
+    }
+}
