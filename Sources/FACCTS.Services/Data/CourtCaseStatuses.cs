694ccb8b4f04f9ded95cf2c8078a7adcb4a59f32
VyacheslavAkeyev <Vyacheslav.Akeyev@opensoftdev.ru>
2013-06-07 17:04:07 +0400

    Additional dlls added.

---
+++ Sources/FACCTS.Services/Data/CourtCaseStatuses.cs
@@ -0,0 +1,32 @@
+﻿using FACCTS.Server.Model.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FACCTS.Services.Data
+{
+    public class CourtCaseStatuses : WebApiClientBase
+    {
+        protected CourtCaseStatuses() : base()
+        {
+
+        }
+
+        private static List<CourtCase> _all;
+        public static List<CourtCase> GetAll()
+        {
+            if (_all == null)
+            {
+                _all = new CourtCaseStatuses().GetCourtCaseStatuses();
+            }
+            return _all;
+        }
+
+        protected List<CourtCase> GetCourtCaseStatuses()
+        {
+            return this.CallServiceGet<List<CourtCase>>("CourtCaseStatuses");
+        }
+    }
+}
