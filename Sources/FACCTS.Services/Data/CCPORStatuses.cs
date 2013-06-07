a3d3641fb8b8419920fdcfa339a7a0e0463457b0
VyacheslavAkeyev <Vyacheslav.Akeyev@opensoftdev.ru>
2013-06-07 18:30:33 +0400

    Class stub added.

---
+++ Sources/FACCTS.Services/Data/CCPORStatuses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FACCTS.Services.Data
+{
+    public class CCPORStatuses
+    {
+        public static List<String> GetAll()
+        {
+            return null;
+        }
+    }
+}
