c6afbd53b05763e1393c6c01a4a594032cdea8d4
TatyanaFedorova <tatyana.fedorova@opensoftdev.ru>
2013-06-14 14:36:53 +0400

    fix

--- Sources/FACCTS.Server.Services/DropCreateFacctsModelChanged.cs
+++ Sources/FACCTS.Server.Services/DropCreateFacctsModelChanged.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace FACCTS.Server.Data
 {
-    public class DropCreateFacctsModelChanged : DropCreateDatabaseIfModelChanges<DatabaseContext>
+    public class DropCreateFacctsModelChanged : DropCreateDatabaseAlways<DatabaseContext>
     {
         protected override void Seed(DatabaseContext context)
         {
