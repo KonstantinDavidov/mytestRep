d7cedd5dd4199d7f65162905dccd57b9b7ede4da
VyacheslavAkeyev <Vyacheslav.Akeyev@opensoftdev.ru>
2013-06-14 21:52:48 +0400

    New enums added to the data model

---
+++ Sources/FACCTS.Server.Model/Enums/FACCTSEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FACCTS.Server.Model.Enums
+{
+    public enum FACCTSEntity
+    {
+        [Description("Person")]
+        Person,
+        [Description("Govt. Agency")]
+        GovtAgency,
+        [Description("Entity")]
+        Entity,
+    }
+}
---
+++ Sources/FACCTS.Server.Model/Enums/Relationship.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FACCTS.Server.Model.Enums
+{
+    public enum Relationship
+    {
+        [Description("Friend")]
+        Friend,
+        [Description("Child")]
+        Child,
+        [Description("Parent")]
+        Parent,
+        [Description("Sibling")]
+        Sibling,
+        [Description("Other Relative")]
+        OtherRelative,
+    }
+}
