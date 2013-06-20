7ef85a11ee5338e66f09cc9b1ce6cf33dc0fe1e5
TatyanaFedorova <tatyana.fedorova@opensoftdev.ru>
2013-06-20 17:27:06 +0400

    added web api controllers for participant role and designation

---
+++ Sources/FACCTS.Server/Controllers/DesignationController.cs
@@ -0,0 +1,35 @@
+﻿using FACCTS.Server.Model.DataModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FACCTS.Server.Controllers
+{
+    [Export]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class DesignationController : ApiControllerBase
+    {
+        // GET api/Designation
+        public IEnumerable<Designation> Get()
+        {
+            return DataManager.DesignationRepository.GetAll();
+        }
+
+        // GET api/Designation/5
+        public Designation Get(int id)
+        {
+            var entity = DataManager.DesignationRepository.GetById(id);
+            if (entity == null)
+            {
+                throw new HttpResponseException(
+                Request.CreateResponse(
+                HttpStatusCode.NotFound));
+            }
+            return entity;
+        }
+    }
+}
