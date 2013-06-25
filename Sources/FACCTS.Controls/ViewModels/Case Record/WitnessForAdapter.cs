c3dc16602aba49317a8fb2312de7213df4562ea3
VyacheslavAkeyev <Vyacheslav.Akeyev@opensoftdev.ru>
2013-06-25 20:08:40 +0400

    WPF client: Witnesses can be added/edited/deleted.

---
+++ Sources/FACCTS.Controls/ViewModels/Case Record/WitnessForAdapter.cs
@@ -0,0 +1,75 @@
+﻿using Faccts.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace FACCTS.Controls.ViewModels
+{
+    public class WitnessForAdapter : ViewModelBase
+    {
+        private bool _isParty1;
+        public WitnessForAdapter(CourtParty courtParty, bool isParty1 = true)
+            : base()
+        {
+            if (courtParty == null)
+            {
+                throw new ArgumentNullException("courtParty");
+            }
+            UnderlyingObject = courtParty;
+            UnderlyingObject.WhenAny(
+                            y => y.FirstName,
+                            y => y.MiddleName,
+                            y => y.LastName,
+                            (x1, x2, x3) => new
+                            {
+                                FirstName = x1,
+                                MiddleName = x2,
+                                LastName = x3,
+                            }
+                            )
+                            .Subscribe(y =>
+                            {
+                                if (string.IsNullOrWhiteSpace(UnderlyingObject.FullName))
+                                {
+                                    DisplayName = string.Format("Witness for: {0}", _isParty1 ? "Party 1" : "Party 2");
+                                }
+                                else
+                                {
+                                    DisplayName = string.Format("Witness for: {0}", UnderlyingObject.FullName);
+                                }
+                            }
+                            );
+            _isParty1 = isParty1;
+        }
+
+        public CourtParty UnderlyingObject
+        {
+            get;
+            private set;
+        }
+
+        public override string DisplayName
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(UnderlyingObject.FullName))
+                {
+                    base.DisplayName = string.Format("Witness for: {0}", _isParty1 ? "Party 1" : "Party 2");
+                }
+                return base.DisplayName;
+            }
+            set
+            {
+                base.DisplayName = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.DisplayName;
+        }
+    }
+}
--- Sources/FACCTS.Controls/ViewModels/Case Record/WitnessInterpereterViewModel.cs
+++ Sources/FACCTS.Controls/ViewModels/Case Record/WitnessInterpereterViewModel.cs
@@ -1,18 +1,89 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using Faccts.Model.Entities;
+using FACCTS.Controls.Events;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ReactiveUI;
+using FACCTS.Services.Dialog;
 
 namespace FACCTS.Controls.ViewModels
 {
     [Export(typeof(WitnessInterpereterViewModel))]
-    public class WitnessInterpereterViewModel : ViewModelBase
+    public partial class WitnessInterpereterViewModel : ViewModelBase, IHandle<CurrentCourtCaseChangedEvent>
     {
-        public WitnessInterpereterViewModel() : base()
+        private IEventAggregator _eventAggregator;
+        private IDialogService _dialogService;
+
+        [ImportingConstructor]
+        public WitnessInterpereterViewModel(
+            IEventAggregator eventAggregator,
+            IDialogService dialogService
+            ) : base()
         {
+            _eventAggregator = eventAggregator;
+            _dialogService = dialogService;
+            _eventAggregator.Subscribe(this);
+            this.WhenAny(x => x.CurrentCourtCase, x => x.Value)
+                .Subscribe(x =>
+                {
+                    this.NotifyOfPropertyChange(() => CaseRecord);
+                    this.NotifyOfPropertyChange(() => WitnessesFor);
+                }
+                );
+
             this.DisplayName = "Withess and Interpreter";
+        }
+
+        public void Handle(CurrentCourtCaseChangedEvent message)
+        {
+            this.CurrentCourtCase = message.CourtCase;
+        }
+
+        public CaseRecord CaseRecord
+        {
+            get
+            {
+                if (CurrentCourtCase == null || CurrentCourtCase.CaseRecord == null)
+                    return null;
+                return CurrentCourtCase.CaseRecord;
+            }
+        }
+
+        public void AddWitness()
+        {
+            var newWitness = new Witnesses()
+                {
+                    CourtParty = CaseRecord.CourtParty,
+                    Designation = DataContainer.Designations.FirstOrDefault(),
+                };
+
+            CaseRecord.Witnesses.Add(newWitness);
+        }
+
+        public void RemoveWitness(Witnesses witness)
+        {
+            if (_dialogService.MessageBox("Do you really want to remove the witness?", "Witness removal", System.Windows.MessageBoxButton.YesNo) == System.Windows.MessageBoxResult.Yes)
+            {
+                CaseRecord.Witnesses.Remove(witness);
+            }
+        }
+
+        public List<WitnessForAdapter> WitnessesFor
+        {
+            get
+            {
+                List<WitnessForAdapter> result = new List<WitnessForAdapter>()
+                {
+                    new WitnessForAdapter(CaseRecord.CourtParty),
+                    new WitnessForAdapter(CaseRecord.CourtParty1, false),
+                };
+
+                return result;
+            }
         }
     }
 }
