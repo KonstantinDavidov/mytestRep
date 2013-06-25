b6db9f6d466e05155e036e85df2c73ba09e121a2
VyacheslavAkeyev <Vyacheslav.Akeyev@opensoftdev.ru>
2013-06-25 12:40:29 +0400

    New template added to FACCTS.Server.Model

---
+++ Sources/FACCTS.Server.Model/Reporting/ModelTemplate.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace FACCTS.Server.Model.ViewModels
+{
+    
+			
+	public partial class NewCourtCaseDialogViewModel
+	{
+		private string _caseNumber;
+		public string CaseNumber
+		{
+			get{return _caseNumber;}
+			set{
+				if(_caseNumber!=value){
+					_caseNumber = value;
+				}
+			}
+		}
+		
+		private bool _isEditing;
+		public bool IsEditing
+		{
+			get{return _isEditing;}
+			set{
+				if(_isEditing!=value){
+					_isEditing = value;
+				}
+			}
+		}
+				
+	}
+	
+}
+
