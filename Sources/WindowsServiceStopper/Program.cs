525b797876a77d608b566d1485bb066972c90f62
ys88 <lubchenkoys@gmail.com>
2013-06-26 17:32:00 +0400

    FACCTS.Server: 1) FACCTS WCFService hosted in FACCTS WindowsService; 2) Added client for FACCTS WCFService; 3) Added deployment for FACCTS WindowsService.

---
+++ Sources/WindowsServiceStopper/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+
+namespace WindowsServiceStopper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string serviceName = "FACCTS.IntegrationScheduler";
+
+            var services = ServiceController.GetServices();
+            if (services.FirstOrDefault(s => s.ServiceName == serviceName) != null)
+            {
+                var controller = new ServiceController(serviceName);
+                controller.Stop();
+                controller.WaitForStatus(ServiceControllerStatus.Stopped);
+
+
+                Process p = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+
+                /*startInfo.Arguments = string.Format("stop {0}", serviceName);
+                startInfo.FileName = "sc.exe";
+                p.StartInfo = startInfo;
+                p.Start();
+                p.WaitForExit();*/
+
+                p = new Process();
+                startInfo = new ProcessStartInfo();
+                startInfo.Arguments = string.Format("delete {0}", serviceName);
+                startInfo.FileName = "sc.exe";
+                p.StartInfo = startInfo;
+                p.Start();
+                p.WaitForExit();
+            }
+        }
+    }
+}
