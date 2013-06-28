b7afd91989776601cd5d898abbb9a88b8f1058c9
TatyanaFedorova <tatyana.fedorova@opensoftdev.ru>
2013-06-28 11:47:52 +0400

    UI

---
+++ Sources/FACCTS.Server.Model/Enums/OrderItemState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FACCTS.Server.Model
+{
+    public enum OrderItemState
+    {
+        [Display(Name = "NotRequested")]
+        NotRequested = 0,
+        [Display(Name = "Denied")]
+        Denied = 1,
+        [Display(Name = "Granted")]
+        Granted = 2
+    }
+}
---
+++ Sources/FACCTS.Server.Model/Reporting/Entities/CaseInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FACCTS.Server.Model.Reporting.Entities
+{
+    public class CaseInfo
+    {
+        public string CaseNumber { get; set; }
+        public bool IsSealed { get; set; }
+        public int CasesOnDocket { get; set; }
+        public int CasesCompleteHead { get; set; }
+        public bool IsTempJudge { get; set; }
+        public string TempJudgeName { get; set; }
+    }
+}
