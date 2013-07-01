5cb42cfd368ef27af4f9f577b3cc52f75dd8040b
ys88 <lubchenkoys@gmail.com>
2013-07-01 17:13:18 +0400

    FACCTS.Controls Added views into Court Orders tab 1) DV110Supervision 2) DV110AttachDV145 3) DV130RestrainingOrder 4) DV130PropertyAndAttorneysFees 5) DV130ChildCustody 6) DV130Supervision 7) DV130FL342ChildSupport 8) DV130AttachDV145

---
+++ Sources/FACCTS.Controls/ViewModels/OutputForms/DV130RestrainingOrderViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FACCTS.Controls.ViewModels
+{
+    public class DV130RestrainingOrderViewModel : ViewModelBase
+    {
+        public DV130RestrainingOrderViewModel()
+        {
+            DisplayName = "DV130 Restraining Order";
+        }
+    }
+}
