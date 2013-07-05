3e0c67d56781df60304c470c9d832345bf0151d9
VyacheslavAkeyev <Vyacheslav.Akeyev@opensoftdev.ru>
2013-07-05 21:12:00 +0400

    Server Installer; the dialog with SQL server options partially implemented.

---
+++ Sources/FACCTS.Server.Model/Enums/Designation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FACCTS.Server.Model.Enums
+{
+    //public enum Designation
+    //{
+    //    [Description("None")]
+    //    None = 0,
+    //    [Description("Plaintiff")]
+    //    Plaintiff = 1,
+    //    [Description("Respondent")]
+    //    Respondent = 2,
+    //}
+}
