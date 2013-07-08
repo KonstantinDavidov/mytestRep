9d3a4723ee329ab1b51164b72ad5b7641df118c2
VyacheslavAkeyev <Vyacheslav.Akeyev@opensoftdev.ru>
2013-07-08 18:00:18 +0400

    WPF client: Generate Court Orders dialog enlarged.

---
+++ Sources/FACCTS.Server.Model/Enums/MasterOrders.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FACCTS.Server.Model.Enums
+{
+    public enum MasterOrders
+    {
+        [Description("DV110 - Temporary restraining Order")]
+        DV110 = 1,
+        [Description("DV130 - Restraining Order")]
+        DV130,
+        [Description("CH110 - Civil Harrasment")]
+        CH110,
+        [Description("Ch130 - Civil Harrasment")]
+        CH130,
+        [Description("EA110 - Eldery Abuse")]
+        EA110,
+        [Description("EA130 - Eldery Abuse")]
+        EA130
+    }
+}
