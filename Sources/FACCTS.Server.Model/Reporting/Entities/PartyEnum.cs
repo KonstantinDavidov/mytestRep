92e3f2a560fc3bf3ce0673dd20099696f64b3152
TatyanaFedorova <tatyana.fedorova@opensoftdev.ru>
2013-07-12 17:55:55 +0400

    Finished CH130

---
+++ Sources/FACCTS.Server.Model/Reporting/Entities/PartyEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FACCTS.Server.Model.Reporting.Entities
+{
+    public enum PartyEnum
+    {
+        Party1 = 0,
+        Party2 = 1
+    }
+}
