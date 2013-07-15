cff0b145914549d4a93b8e9c2035d67174946403
VyacheslavAkeyev <Vyacheslav.Akeyev@opensoftdev.ru>
2013-07-15 14:04:02 +0400

    Uncommited changes added.

---
+++ Sources/FACCTS.Server.Services/FacctsDataRepositoryExtensions.cs
@@ -0,0 +1,30 @@
+﻿using FACCTS.Server.DataContracts;
+using FACCTS.Server.Model.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FACCTS.Server.Data
+{
+    public static class FacctsDataRepositoryExtensions
+    {
+        public static void SaveData<T>(this IFacctsDataRepository<T> repository, T entity)
+            where T : BaseEntity
+        {
+            switch(entity.State)
+            {
+                case ObjectState.Added:
+                    repository.Insert(entity);
+                    break;
+                case ObjectState.Deleted:
+                    repository.Delete(entity);
+                    break;
+                case ObjectState.Modified:
+                    repository.Update(entity);
+                    break;
+            }
+        }
+    }
+}
---
+++ Sources/FACCTS.Server.Services/IFacctsStandardRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FACCTS.Server.Data
+{
+    class IFacctsStandardRepository<T>
+    {
+    }
+}
