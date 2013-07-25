8a94055361d270e3c59450bba8780a40d1540838
TatyanaFedorova <tatyana.fedorova@opensoftdev.ru>
2013-07-25 13:17:14 +0400

    DV145 added

---
+++ Sources/FACCTS.Server.Model/Reporting/Entities/RestrainedLocations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FACCTS.Server.Model.Reporting.Entities
+{
+    [Flags]
+    public enum RestrainedLocations
+    {
+        [Description("County")]
+        County = 0,
+        [Description("California")]
+        California = 0x0001,
+        [Description("United States")]
+        US = 0x0002,
+        [Description("Other")]
+        Other = 0x0004
+    }
+}
