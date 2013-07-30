7759d4c60d048ddb48736d70d93cca2f5589eac0
TatyanaFedorova <tatyana.fedorova@opensoftdev.ru>
2013-07-30 15:31:15 +0400

    FL344 added

---
+++ Sources/FACCTS.Server.Model/Reporting/Entities/DebtItem.cs
@@ -0,0 +1,17 @@
+﻿using FACCTS.Server.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FACCTS.Server.Model.Reporting.Entities
+{
+    public class DebtItem
+    {
+        public decimal TotalDebt { get; set; }
+        public decimal AmountOfPayments { get; set; }
+        public Designation PayTo { get; set; }
+        public Designation PaidBy { get; set; }
+    }
+}
