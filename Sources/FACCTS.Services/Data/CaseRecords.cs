0b52f2f624f78ef2b2ead03d77d6ff1ae5d64837
VyacheslavAkeyev <Vyacheslav.Akeyev@opensoftdev.ru>
2013-07-30 13:45:50 +0400

    Missing files added.

---
+++ Sources/FACCTS.Services/Data/CaseRecords.cs
@@ -0,0 +1,18 @@
+﻿using FACCTS.Server.Model.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FACCTS.Services.Data
+{
+    public class CaseRecords : WebApiClientBase
+    {
+        //public static Faccts.Model.Entities.CaseRecord AddCaseRecord(long courtCaseId, Faccts.Model.Entities.CaseRecord caseRecord)
+        //{
+        //    CaseRecord dto = caseRecord.ToDTO();
+        //    var updatedRecord = new CaseRecords().CallServicePost<CaseRecord, CaseRecord>("CaseRecords", dto);
+        //}
+    }
+}
