e07230bdd63ae5b2345f13188e1be4d65b4008b2
VyacheslavAkeyev <Vyacheslav.Akeyev@opensoftdev.ru>
2013-08-03 19:13:39 +0400

    WPF client: an additional control for displaying the Case history in Case Status screen added.

---
+++ Sources/FACCTS.Controls/TreeListView/Converters.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FACCTS.Controls.TreeListView
+{
+    // <summary>
+    /// Convert Level to left margin
+    /// </summary>
+    public class LevelToIndentConverter : IValueConverter
+    {
+        private const double IndentSize = 19.0;
+
+        public object Convert(object o, Type type, object parameter, CultureInfo culture)
+        {
+            return new Thickness((int)o * IndentSize, 0, 0, 0);
+        }
+
+        public object ConvertBack(object o, Type type, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    public class CanExpandConverter : IValueConverter
+    {
+        public object Convert(object o, Type type, object parameter, CultureInfo culture)
+        {
+            if ((bool)o)
+                return Visibility.Visible;
+            else
+                return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object o, Type type, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
