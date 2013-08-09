44cc0fa3a49326e6ae04ba6f5e0e033321ea7cb3
YuryLubchenko <lubchenkoys@gmail.com>
2013-08-09 14:58:45 +0400

    FACCTS.Client: binding DV130 Property and attorney's fees view to model

--- Sources/FACCTS.Controls/ViewModels/OutputForms/DV130PropertyAndAttorneysFeesViewModel.cs
+++ Sources/FACCTS.Controls/ViewModels/OutputForms/DV130PropertyAndAttorneysFeesViewModel.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Faccts.Model.Entities.Reporting;
+using FACCTS.Server.Model.DataModel;
+using FACCTS.Server.Model.Enums;
 
 namespace FACCTS.Controls.ViewModels
 {
-    public class DV130PropertyAndAttorneysFeesViewModel :ViewModelBase
+    public class DV130PropertyAndAttorneysFeesViewModel :CourtOrderBaseViewModel<DV130>
     {
         public DV130PropertyAndAttorneysFeesViewModel()
         {
             DisplayName = "DV130 Restraining Order - Property and Attorney's Fees";
+            OrderType = CourtOrdersTypes.DV130;
         }
     }
 }
